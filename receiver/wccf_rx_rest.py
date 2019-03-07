7c125c3d23284de07a31aa08a1e10f8e42f2b61d
jredmore <48229065+jredmore@users.noreply.github.com>
2019-03-07 14:32:36 -0700

    Update wccf_rx_rest.py

--- receiver/wccf_rx_rest.py
+++ receiver/wccf_rx_rest.py
@@ -1,3 +1,16 @@
+# Copyright (c) 2019 Cable Television Laboratories, Inc.
+# Licensed under the BSD+Patent (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#          https://opensource.org/licenses/BSDplusPatent
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from bottle import abort,run,install,request,route,response
 from datetime import datetime
 from functools import wraps
