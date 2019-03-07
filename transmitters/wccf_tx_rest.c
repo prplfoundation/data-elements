d29149f89b20b9d302c5a0ab9fcb4132a3bf8aa0
jredmore <48229065+jredmore@users.noreply.github.com>
2019-03-07 14:25:26 -0700

    Update wccf_tx_rest.c

--- transmitters/wccf_tx_rest.c
+++ transmitters/wccf_tx_rest.c
@@ -1,3 +1,4 @@
+/*
 # Copyright (c) 2019 Cable Television Laboratories, Inc.
 # Licensed under the BSD+Patent (the "License");
 # you may not use this file except in compliance with the License.
@@ -10,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+*/
 
 #include "wccf_tx_rest.h"
 
